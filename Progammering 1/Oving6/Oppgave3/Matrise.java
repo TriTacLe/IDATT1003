ae8fb5767342818d3f6b4234938c90ef65a4e308
TriTacLe <tritac.le@gmail.com>
2024-10-03 17:36:42 +0200

    w

--- Progammering 1/Oving6/Oppgave3/Matrise.java
+++ Progammering 1/Oving6/Oppgave3/Matrise.java
@@ -3,7 +3,7 @@
 public class Matrise {
     int [][] matrise;
 
-    
+
     public void printMatrix(int matrix[][]) {
         for (int i = 0; i < matrix.length; i++){ // length returns number of rows
 	    	for (int j = 0; j < matrix[i].length; j++) { // here length returns number of columns corresponding to current row 
