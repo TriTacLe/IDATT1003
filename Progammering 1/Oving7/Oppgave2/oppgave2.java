d3ee1460a9f39be7316d2acd7f5450f228968588
TriTacLe <tritac.le@gmail.com>
2024-10-17 03:19:50 +0200

    ingen anelse hva som skjedde

--- Progammering 1/Oving7/Oppgave2/oppgave2.java
+++ Progammering 1/Oving7/Oppgave2/oppgave2.java
@@ -1,5 +1,19 @@
-package Oppgave2;
-
 public class oppgave2 {
-    
+    public static void main(String[] args) {
+
+        tekstbehandling tekst = new tekstbehandling("æøåæøå. Hello. World. æøåæøå.");
+
+        System.out.println("Selve teksten: " + tekst.getTekst());
+        System.out.println("Teksten i storebokstaver: " + tekst.getTekstUppercase());
+
+        System.out.println("Antall ord: " + tekst.numWords());
+        System.out.println("Gjennomsnittslig ordlengde " + tekst.averageWordLength());
+
+        String OrgOrd = "æøåæøå.";
+        String nyttOrd = "ø æ å ø æ å";
+
+        System.out.println("Byttet ut: " + "\"" + OrgOrd + "\"" +" med " + "\"" + nyttOrd + "\"" + " Endret tekst: " + tekst.replaceWord(OrgOrd,nyttOrd)); //OrgOrd må inneholde ordet og skilletegnet bak 
+
+        System.out.println("Antall ord per periode(setning): " + tekst.numWordsPeriod());
+    }
 }
