8e3ad8fd58ae3498139ffe0f259dce3bd073fc32
TriTacLe <tritac.le@gmail.com>
2024-10-17 03:39:06 +0200

    commit på intellij

---
+++ Progammering 1/Oving8/src/ArbTaker.java
@@ -0,0 +1,10 @@
+public class ArbTaker {
+
+    private Person personalia;
+    private int arbeidstakernr;
+    private int ansettelsesår;
+    private int månedslønn;
+    private double skatteprosent;
+
+
+}
--- Progammering 1/Oving8/src/Person.java
+++ Progammering 1/Oving8/src/Person.java
@@ -1,2 +1,18 @@
 public class Person {
+    private String fornavn;
+    private String etternavn;
+    private int alder;
+
+    public getFornavn(){
+        return fornavn;
+    }
+
+    public getEtternavn(){
+        return etternavn;
+    }
+
+    public getAlder(){
+        return alder;
+    }
+
 }
