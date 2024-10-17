ae5601cfd913fda69e57248ec52fa476764e14e5
TriTacLe <tritac.le@gmail.com>
2024-10-17 03:55:08 +0200

    blir det commited?

--- Progammering 1/Oving8/src/ArbTaker.java
+++ Progammering 1/Oving8/src/ArbTaker.java
@@ -1,17 +1,35 @@
 public class ArbTaker {
-
     private Person personalia;
     private int arbeidstakernr;
     private int ansettelsesår;
-    private int månedslønn;
+    private double månedslønn;
     private double skatteprosent;
 
+    public ArbTaker(Person personalia, int arbeidstakernr, int ansettelsesår, double månedslønn, double skatteprosent) {
+        this.personalia = personalia;
+        this.arbeidstakernr = arbeidstakernr;
+        this.ansettelsesår = ansettelsesår;
+        this.månedslønn = månedslønn;
+        this.skatteprosent = skatteprosent;
+    }
+
+    /**
+     * getters
+     * @return
+     */
     public Person getPersonalia(){
         return personalia;
     }
-
     public int getArbeidstakernr(){
         return arbeidstakernr;
     }
-
+    public int getAnsettelsesår(){
+        return ansettelsesår;
+    }
+    public double getMånedslønn(){
+        return månedslønn;
+    }
+    public double getSkatteprosent(){
+        return skatteprosent;
+    }
 }
--- Progammering 1/Oving8/src/Person.java
+++ Progammering 1/Oving8/src/Person.java
@@ -3,6 +3,12 @@ public class Person {
     private String etternavn;
     private int alder;
 
+    public Person(String fornavn, String etternavn, int alder){
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.alder = alder;
+    }
+
     public String getFornavn(){
         return fornavn;
     }
